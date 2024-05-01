ac4b815c01a951afd7e2eee2627116775b6a5e9d
Kao6023 <157576769+Kao6023@users.noreply.github.com>
2024-05-01 09:56:13 +0800

    add Drawing.py file
    1. 完成Drawing.py額外繪圖程式
    2. main.py沒有繪圖功能

--- Drawing.py
+++ Drawing.py
@@ -6,13 +6,26 @@ from Dual_axis import Dual_Axis
 
 class Waveform():
     def __init__(self):
-        self.date = datetime.now()
-    def maindata_list(self): 
-        self.demand_power_list = pd.read_excel("schedule_EdReg.xlsx")["Energy_transfer(kW)"].to_list()
-        self.time_list = pd.read_excel("schedule_EdReg.xlsx")["Time"].to_list()
-        return self.energytransfer_list, self.time_list
+        today_date = datetime.today().strftime('%Y-%m-%d')
+        self.file = f"{today_date}test.csv"
+        print(self.file)
+
+    def main_list(self): 
+        self.grid_frequency_list = pd.read_csv(self.file)["Grid_frequency"].to_list()
+        self.demand_power_list = pd.read_csv(self.file)["Demand_power"].to_list()
+        self.active_power_list = pd.read_csv(self.file)["Active_power"].to_list()
     
-
-
-if __name__ == '__name__':
-    print(datetime.now())
+    def show(self):
+        dual_axis = Dual_Axis()
+        dual_axis.plot(self.grid_frequency_list, self.demand_power_list, self.active_power_list,
+                    'r-','b-','g-',"grid_frequency",'Demand_power','Active_power',None ,(59.4,60.6), (-12.5,12.5), (-10,10))
+        dual_axis.draw(60)
+        
+    
+if __name__ == "__main__":
+    waveform = Waveform()
+    start = time.time()
+    waveform.main_list()
+    waveform.show()
+    end = time.time()
+    print(end-start)
